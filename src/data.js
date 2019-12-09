048bb45d068b199c11c598c6d162a1a0b4927d24
Marcelo Dapper <marcelo.dapper@gmail.com>
2019-12-09 18:17:34 -0300

    Add onDragEnd implementation

--- src/data.js
+++ src/data.js
@@ -17,12 +17,12 @@ export const initialData = {
       content: 'Cook dinner',
     },
   ],
-  columns: [
-    {
+  columns: {
+    'column-1': {
       id: 'column-1',
       title: 'To do',
       taskIds: ['task-1', 'task-2', 'task-3', 'task-4'],
     },
-  ],
+  },
   columnOrder: ['column-1'],
 };
--- src/index.js
+++ src/index.js
@@ -11,14 +11,45 @@ class App extends React.Component {
   state = initialData;
 
   onDragEnd = result => {
-    // TODO
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = this.state.columns[source.droppableId];
+    const newTaskIds = [...column.taskIds];
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    };
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    this.setState(newState);
   };
 
   render() {
     return (
       <DragDropContext onDragEnd={this.onDragEnd}>
         {this.state.columnOrder.map(columnId => {
-          const column = this.state.columns.find(elem => elem.id === columnId);
+          const column = this.state.columns[columnId];
           const tasks = column.taskIds.map(taskId =>
             this.state.tasks.find(elem => elem.id === taskId)
           );
